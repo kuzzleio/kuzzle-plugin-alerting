bef82ad99b9377128bb4e81a3a782457d02bc6f4
Thomas Falcone <thomas.falcone06@gmail.com>
2019-07-15 15:52:58 +0200

    Add mapping for alarms

--- features/fixtures/mappings.js
+++ features/fixtures/mappings.js
@@ -1,4 +1,65 @@
 // See https://docs.kuzzle.io/api/1/controller-admin/load-mappings/
 
+// Alarm mapping
 module.exports = {
-};
+  'name': {
+    'type': 'text',
+  },
+  'filterId': {
+    'type': 'text',
+    'fields': {
+      'keyword': {
+        'type': 'keyword',
+        'ignore_above': 256
+      }
+    }
+  },
+  'condition': {
+    'properties': {
+      'type': {
+        'type': 'text',
+        'fields': {
+          'keyword': {
+            'type': 'keyword',
+            'ignore_above': 256
+          }
+        }
+      },
+      'index': {
+        'type': 'text'
+      },
+      'collection': {
+        'type': 'text'
+      },
+      'filter': {
+        'type': 'percolator'
+      },
+      "scope": {
+        'type': 'text'
+      },
+      "duration": {
+        'type': 'text'
+      }
+    }
+  },
+  'actions': {
+    'properties': {
+      'request': {
+        'properties': {
+          'controller': {
+            'type': 'text'
+          },
+          'action': {
+            'type': 'text'
+          },
+          'index': {
+            'type': 'text'
+          },
+          'collection': {
+            'type': 'text'
+          }
+        }
+      }
+    }
+  }
+}
--- features/support/hooks.js
+++ features/support/hooks.js
@@ -8,19 +8,9 @@ const
   World = require('./world');
 
 BeforeAll(async function () {
-  const
-    world = new World({ });
-
-  world.kuzzle = new Kuzzle(
-    new WebSocket(world.host, { port: world.port })
-  );
 });
 
 Before(async function () {
-  // const
-  //   fixtures = Object.assign({}, testFixtures),
-  //   mappings = Object.assign({}, testMappings);
-
   this.kuzzle = new Kuzzle(
     new WebSocket(this.host, { port: this.port })
   );
